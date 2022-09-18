8c51cc5f72e4159d6040ceb11d13acfe5b348e29
chhotu marandi <cmarandi29@gmail.com>
2022-09-18 18:25:28 +0530

    clicked on button pop up

--- index.js
+++ index.js
@@ -9,4 +9,8 @@ const p_img_elem = document.querySelectorAll('.img-overlay');
 p_btns.addEventListener('click', (e) => {
   const p_btn_clicked = e.target;
   console.log(p_btn_clicked);
+
+  p_btn.forEach((currElem) => currElem.classList.remove('p-btn-active'));
+
+  p_btn_clicked.classList.add('p-btn-active');
 });
