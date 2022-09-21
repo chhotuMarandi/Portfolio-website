b62672170eb59f0a07cdf274361111c65efaf032
chhotu marandi <cmarandi29@gmail.com>
2022-09-21 11:57:37 +0530

    auto play feature add in swiper

--- index.js
+++ index.js
@@ -60,6 +60,9 @@ counters.forEach((counter) => {
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 3,
   spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
